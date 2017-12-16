6819e14538f9a4fd7cec9345d47ddbf66cdcd061
Charlotte Fereday <cbrfereday@gmail.com>
2017-12-16 16:49:35 +0000

    [Charlotte] refactors stripe constants

--- frontend/src/constants/stripe.js
+++ frontend/src/constants/stripe.js
@@ -2,4 +2,4 @@ const STRIPE PUBLISHABLE = process.env.NODE_ENV === 'production'
     ? 'pk_test_MY_PUBLISHABLE KEY';
     : 'pk_test_YGlZJj90lqY3KN8EAZNW34cW';
 
-export default STRIPE_PUBLISHABLE;
+export default  STRIPE_PUBLISHABLE;
