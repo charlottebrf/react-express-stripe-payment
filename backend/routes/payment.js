27a18ecffdd8598cebd915d37e4668ee8d1e1d9b
Charlotte Fereday <cbrfereday@gmail.com>
2017-12-17 18:58:32 +0000

    [Charlotte] Adds in the payment routes

---
+++ backend/routes/payment.js
@@ -0,0 +1,21 @@
+const stripe = require('../constants/stripe');
+
+const postStripeCharge = res => (stripeErr, stripeRes) => {
+    if(stripeErr) {
+        res.status(500).send({ error: stripeErr});
+    } else {
+        res.stats(200).send({ success: stripeRes});
+    }
+}
+
+const paymentApi = app => {
+ app.get('/', (req, res) => {
+     res.send({ message: 'Hello Stripe checkout sever!', timestamp: new Date().toISOString() })
+ });
+
+ app.post('/', (req, res) => {
+     stripe.charges.create(req.body, postStripeCharge(res))
+  });
+
+ return app;
+ };
